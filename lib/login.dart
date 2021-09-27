a9c6344ab65076af6ce99b430373d0e435335cc0
Inderpal Singh <47072789+gandaleve@users.noreply.github.com>
2021-09-27 09:54:14 -0700

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -13,26 +13,20 @@ class _LoginViewState extends State<LoginView> {
   late String _email;
   late String _password;
 
-  void validate() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Container(
-          padding: EdgeInsets.all(16),
           child: Form(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
                 ),
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
                   obscureText: true,
                 ),
-                ElevatedButton(onPressed: validate, child: Text('Login'))
               ],
             ),
           )),
