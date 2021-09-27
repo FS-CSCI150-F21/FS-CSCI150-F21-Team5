57e5db6a7418f668dc1dc7d47563a1defc3c1164
Inderpal Singh <47072789+gandaleve@users.noreply.github.com>
2021-09-27 09:55:51 -0700

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -10,23 +10,57 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final formKey = GlobalKey<FormState>();
+
   late String _email;
   late String _password;
 
+  void validate() {
+    final form = formKey.currentState;
+    if (form!.validate()) {
+      form.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Container(
+          padding: const EdgeInsets.all(30),
           child: Form(
+            key: formKey,
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Email cannot be empty' : null,
+                  onSaved: (value) => _email = value!,
                 ),
+                const SizedBox(height: 20),
                 TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
                   obscureText: true,
+                  validator: (value) =>
+                      value!.isEmpty ? 'Password cannot be empty' : null,
+                  onSaved: (value) => _password = value!,
                 ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: validate,
+                  child: const Text('Login'),
+                )
               ],
             ),
           )),
