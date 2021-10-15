0dff01e1933f00a259392e572a676e4d490e9f3d
Inderpal Singh <inderpal.singh.sarai@icloud.com>
2021-10-15 14:35:26 -0700

    added documentation

--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -5,6 +5,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shakshuka/services/firestore_constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// this enum will be used to track the login state of the user
 enum Status {
   uninitialized,
   authenticated,
@@ -12,7 +13,10 @@ enum Status {
   authenticateError,
   authenticateCanceled,
 }
+//AuthProvider is a changeNotifer which allows other objects to subscribe to listen for changes
+// when the users state changes all listeners will be notifed
 
+// this will act as out basic state mangment solution
 class AuthProvider extends ChangeNotifier {
   final GoogleSignIn googleSignIn;
   final FirebaseAuth firebaseAuth;
@@ -30,10 +34,12 @@ class AuthProvider extends ChangeNotifier {
     required this.firebaseFirestore,
   });
 
+  //retrives firebaseUserId from local cache instead of from the cloud
   String? getFirebaseUserId() {
     return prefs.getString(FirestoreConstants.id);
   }
 
+  // checks the google login state
   Future<bool> isLoggedIn() async {
     bool isLoggedIn = await googleSignIn.isSignedIn();
     if (isLoggedIn &&
@@ -44,10 +50,14 @@ class AuthProvider extends ChangeNotifier {
     }
   }
 
+  // this function does all the heavy lifting for signing google users
+  // if the user does not have an account it register them for one
+  // however if the user does have an account already it signs them in
   Future<bool> handelSignIn() async {
     _status = Status.authenticating;
     notifyListeners();
 
+    // wait for a response to sign in with google and stores the results of that action in the final variable AuthCredential
     GoogleSignInAccount? googleUser = await googleSignIn.signIn();
     if (googleUser != null) {
       GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
@@ -55,17 +65,18 @@ class AuthProvider extends ChangeNotifier {
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-
+      // then take those google user credientials and convert it into a firebase user
       User? firebaseUser =
           (await firebaseAuth.signInWithCredential(credential)).user;
-
+      // if the firebase user is not null search out database for the user and stoer the result in document
       if (firebaseUser != null) {
         final QuerySnapshot result = await firebaseFirestore
             .collection(FirestoreConstants.pathUserCollection)
             .where(FirestoreConstants.id, isEqualTo: firebaseUser.uid)
             .get();
         final List<DocumentSnapshot> document = result.docs;
-        if (document.length == 0) {
+        // if the document is empty  add the user to you firebase database as a new user
+        if (document.isEmpty) {
           firebaseFirestore
               .collection(FirestoreConstants.pathUserCollection)
               .doc(firebaseUser.uid)
@@ -74,12 +85,14 @@ class AuthProvider extends ChangeNotifier {
             FirestoreConstants.id: firebaseUser.uid,
             'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
           });
+          // stores the user to local prefs
           User? currentUser = firebaseUser;
           await prefs.setString(FirestoreConstants.id, currentUser.uid);
           await prefs.setString(
               FirestoreConstants.nickname, currentUser.displayName ?? "");
         } else {
           DocumentSnapshot documentSnapshot = document[0];
+          //TODO implement fetching pre-exsisting userdata from firestore
         }
         _status = Status.authenticated;
         notifyListeners();
