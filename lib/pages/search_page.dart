2369a3d4b29663eae7c1b047039f7406d1cba3f0
Juan Mendoza <Juanmendoza97@mail.fresnostate.edu>
2021-10-19 16:06:05 -0700

    added comment

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// Juan is building this
+
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
   @override
