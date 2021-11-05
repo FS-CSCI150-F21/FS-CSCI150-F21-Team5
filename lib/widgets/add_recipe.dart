77bc7159161e1495ffff80f6255ddbdae6947845
Inderpal Singh <inderpal.singh.sarai@icloud.com>
2021-11-05 11:13:00 -0700

    dismiss input on scroll

--- lib/widgets/add_recipe.dart
+++ lib/widgets/add_recipe.dart
@@ -83,193 +83,216 @@ class _AddRecipeState extends State<AddRecipe> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: Align(
-                  alignment: Alignment(-0.9, 1.0),
-                  child: Text(
-                    'Title',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-                  )),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: 325,
-              child: TextField(
-                controller: titleFieldValueHolder,
-                autocorrect: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter Title',
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey.shade400,
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color.fromRGBO(199, 40, 13, 1),
-                      width: 2.0,
-                    ),
-                  ),
-                ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onPanDown: (_) {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: Align(
+                    alignment: Alignment(-0.9, 1.0),
+                    child: Text(
+                      'Title',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                    )),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              height: 160,
-              width: 160,
-              child: image != null
-                  ? Image.file(
-                      image!,
-                      width: 160,
-                      height: 160,
-                    )
-                  : const FlutterLogo(
-                      size: 160,
-                    ),
-            ),
-            SizedBox(
-              width: 220,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 199, 40, 13),
-                  padding: const EdgeInsets.all(12),
-                  textStyle: const TextStyle(fontSize: 16),
-                ),
-                child: const Text('Add Image'),
-                onPressed: () => _pickImage(ImageSource.gallery),
+              const SizedBox(
+                height: 30,
               ),
-            ),
-            const SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: Align(
-                  alignment: Alignment(-0.9, 1.0),
-                  child: Text(
-                    'Durration',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-                  )),
-            ),
-            SizedBox(
-                child: DurationPicker(
-              duration: _duration,
-              onChange: (val) {
-                setState(() => _duration = val);
-              },
-              snapToMins: 5.0,
-            )),
-            const SizedBox(height: 30),
-            SizedBox(
+              SizedBox(
+                width: 325,
                 child: TextField(
-              controller: textFieldValueHolder,
-              autocorrect: true,
-              decoration: InputDecoration(
-                hintText: 'Enter Ingredient',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.add),
+                  controller: titleFieldValueHolder,
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Title',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color.fromRGBO(199, 40, 13, 1),
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 160,
+                width: 160,
+                child: image != null
+                    ? Image.file(
+                        image!,
+                        width: 160,
+                        height: 160,
+                      )
+                    : const FlutterLogo(
+                        size: 160,
+                      ),
+              ),
+              SizedBox(
+                width: 220,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 199, 40, 13),
+                    padding: const EdgeInsets.all(12),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  child: const Text('Add Image'),
+                  onPressed: () => _pickImage(ImageSource.gallery),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: Align(
+                    alignment: Alignment(-0.9, 1.0),
+                    child: Text(
+                      'Durration',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                    )),
+              ),
+              SizedBox(
+                  child: DurationPicker(
+                duration: _duration,
+                onChange: (val) {
+                  setState(() => _duration = val);
+                },
+                snapToMins: 5.0,
+              )),
+              const SizedBox(height: 30),
+              SizedBox(
+                  width: 325,
+                  child: TextField(
+                    controller: textFieldValueHolder,
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade400,
+                          width: 1.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(199, 40, 13, 1),
+                          width: 2.0,
+                        ),
+                      ),
+                      hintText: 'Enter Ingredient',
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: addIngredient,
+                      ),
+                    ),
+                  )),
+              const SizedBox(height: 30),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 199, 40, 13),
+                    padding: const EdgeInsets.all(12),
+                    textStyle: const TextStyle(fontSize: 22),
+                  ),
+                  child: const Text('Add Value To String Array'),
                   onPressed: addIngredient,
                 ),
               ),
-            )),
-            const SizedBox(height: 30),
-            SizedBox(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 199, 40, 13),
-                  padding: const EdgeInsets.all(12),
-                  textStyle: const TextStyle(fontSize: 22),
-                ),
-                child: const Text('Add Value To String Array'),
-                onPressed: addIngredient,
-              ),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              child: ReorderableListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                children: <Widget>[
-                  for (int index = 0; index < data.length; index++)
-                    Card(
-                      color: Colors.amberAccent.shade100,
-                      key: ValueKey(index),
-                      child: ListTile(
-                        key: Key('$index'),
-                        title: Text('Item ${data[index]}'),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ReorderableListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  children: <Widget>[
+                    for (int index = 0; index < data.length; index++)
+                      Card(
+                        color: Colors.amberAccent.shade100,
+                        key: ValueKey(index),
+                        child: ListTile(
+                          key: Key('$index'),
+                          title: Text('Item ${data[index]}'),
+                        ),
                       ),
-                    ),
-                ],
-                onReorder: (int oldIndex, int newIndex) {
-                  setState(() {
-                    if (oldIndex < newIndex) {
-                      newIndex -= 1;
-                    }
-                    final String item = data.removeAt(oldIndex);
-                    data.insert(newIndex, item);
-                  });
-                },
-              ),
-            ),
-            SizedBox(
-                child: TextField(
-              controller: stepsFieldValueHolder,
-              autocorrect: true,
-              decoration: const InputDecoration(hintText: 'Enter Step'),
-            )),
-            const SizedBox(height: 30),
-            SizedBox(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 199, 40, 13),
-                  padding: const EdgeInsets.all(12),
-                  textStyle: const TextStyle(fontSize: 22),
+                  ],
+                  onReorder: (int oldIndex, int newIndex) {
+                    setState(() {
+                      if (oldIndex < newIndex) {
+                        newIndex -= 1;
+                      }
+                      final String item = data.removeAt(oldIndex);
+                      data.insert(newIndex, item);
+                    });
+                  },
                 ),
-                child: const Text('Add Step'),
-                onPressed: addStep,
               ),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              child: ReorderableListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                children: <Widget>[
-                  for (int index = 0; index < steps.length; index++)
-                    Card(
-                      color: Colors.amberAccent.shade100,
-                      key: ValueKey(index),
-                      child: ListTile(
-                        key: Key('$index'),
-                        title: Text('Item ${steps[index]}'),
+              SizedBox(
+                  child: TextField(
+                controller: stepsFieldValueHolder,
+                autocorrect: true,
+                decoration: const InputDecoration(hintText: 'Enter Step'),
+              )),
+              const SizedBox(height: 30),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 199, 40, 13),
+                    padding: const EdgeInsets.all(12),
+                    textStyle: const TextStyle(fontSize: 22),
+                  ),
+                  child: const Text('Add Step'),
+                  onPressed: addStep,
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                child: ReorderableListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  children: <Widget>[
+                    for (int index = 0; index < steps.length; index++)
+                      Card(
+                        color: Colors.amberAccent.shade100,
+                        key: ValueKey(index),
+                        child: ListTile(
+                          key: Key('$index'),
+                          title: Text('Item ${steps[index]}'),
+                        ),
                       ),
-                    ),
-                ],
-                onReorder: (int oldIndex, int newIndex) {
-                  setState(() {
-                    if (oldIndex < newIndex) {
-                      newIndex -= 1;
-                    }
-                    final String item = steps.removeAt(oldIndex);
-                    steps.insert(newIndex, item);
-                  });
-                },
+                  ],
+                  onReorder: (int oldIndex, int newIndex) {
+                    setState(() {
+                      if (oldIndex < newIndex) {
+                        newIndex -= 1;
+                      }
+                      final String item = steps.removeAt(oldIndex);
+                      steps.insert(newIndex, item);
+                    });
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
