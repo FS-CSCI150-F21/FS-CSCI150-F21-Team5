e578b7332bd7a1a48c1b89435f7b719814783882
Inderpal Singh <gandaleve@gmail.com>
2021-12-03 10:44:57 -0800

    Fully implmented grocery list

--- lib/screens/grocery_page.dart
+++ lib/screens/grocery_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,99 +12,108 @@ class GroceryPage extends StatefulWidget {
 }
 
 class _GroceryPageState extends State<GroceryPage> {
-  String groceries = '';
+  String grocerie = '';
 
-  createTodos() {
+  createGrocery() {
     DocumentReference documentReference =
-        FirebaseFirestore.instance.collection("MyTodos").doc(groceries);
+        FirebaseFirestore.instance.collection("groceries").doc(grocerie);
 
     //Map
-    Map<String, String> todos = {"todoTitle": groceries};
-
-    documentReference.set(todos).whenComplete(() {
-      print("$groceries created");
-    });
+    Map<String, String> groceries = {"groceriesTitle": grocerie};
   }
 
-  deleteTodos(item) {
+  deleteGrocery(item) {
     DocumentReference documentReference =
-        FirebaseFirestore.instance.collection("MyTodos").doc(item);
-
-    documentReference.delete().whenComplete(() {
-      print("$item deleted");
-    });
+        FirebaseFirestore.instance.collection("groceries").doc(item);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("mytodos"),
+        title: const Text("Groceries"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  title: const Text("Add Todolist"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      groceries = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    ElevatedButton(
-                        onPressed: () {
-                          createTodos();
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                title: const Text("Add grocery"),
+                content: TextField(
+                  onChanged: (String value) {
+                    grocerie = value;
+                  },
+                ),
+                actions: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 199, 40, 13),
+                      padding: const EdgeInsets.all(12),
+                      textStyle: const TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      createGrocery();
 
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Add"))
-                  ],
-                );
-              });
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Save"),
+                  )
+                ],
+              );
+            },
+          );
         },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
+        label: const Text('Add'),
+        icon: const Icon(Icons.add),
+        backgroundColor: const Color.fromARGB(255, 199, 40, 13),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16.0)),
         ),
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
+          stream:
+              FirebaseFirestore.instance.collection("groceries").snapshots(),
           builder: (context, snapshots) {
             if (snapshots.hasData) {
               return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshots.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot documentSnapshot =
-                        snapshots.data!.docs[index];
-                    return Dismissible(
-                        onDismissed: (direction) {
-                          deleteTodos(documentSnapshot["todoTitle"]);
-                        },
-                        key: Key(documentSnapshot["todoTitle"]),
-                        child: Card(
-                          elevation: 4,
-                          margin: const EdgeInsets.all(8),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          child: ListTile(
-                            title: Text(documentSnapshot["todoTitle"]),
-                            trailing: IconButton(
-                                icon: const Icon(
-                                  Icons.delete,
-                                  color: Colors.red,
-                                ),
-                                onPressed: () {
-                                  deleteTodos(documentSnapshot["todoTitle"]);
-                                }),
-                          ),
-                        ));
-                  });
+                shrinkWrap: true,
+                itemCount: snapshots.data!.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot documentSnapshot =
+                      snapshots.data!.docs[index];
+                  return Dismissible(
+                    onDismissed: (direction) {
+                      deleteGrocery(documentSnapshot["groceriesTitle"]);
+                    },
+                    key: Key(documentSnapshot["groceriesTitle"]),
+                    child: Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.all(8),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      child: ListTile(
+                        title: Text(documentSnapshot["groceriesTitle"]),
+                        trailing: IconButton(
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              deleteGrocery(documentSnapshot["groceriesTitle"]);
+                            }),
+                      ),
+                    ),
+                  );
+                },
+              );
             } else {
               return const Align(
                 alignment: FractionalOffset.bottomCenter,
